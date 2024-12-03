861584e6d5060b7816f727355c1de94f51cc7cd2
Tanjir Al Mamun <tanjir.almamun@outlook.com>
2024-12-03 22:27:09 +0600

    updated author uri and pot file

--- trademate.php
+++ trademate.php
@@ -8,7 +8,7 @@
  * Requires at least: 5.2
  * Requires PHP:      7.4
  * Author:            WPIntegrity
- * Author URI:        https://wpintegrity.com/trademate/
+ * Author URI:        https://wpintegrity.com/
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       trademate
